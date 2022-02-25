05a3bb8e6635e3dee4baf5c097fcda0d8a1a8641
Evgeniy Khromov <ex.codding@gmail.com>
2022-02-25 13:17:18 +0300

    defects are fixed

--- main.py
+++ main.py
@@ -11,10 +11,12 @@ class YaUploader:
 
     def upload(self, file_path, file):
         href = self._get_upload_link(disk_file_path=file_path).get("href", "")
+        if href == '':
+            return
         response = requests.put(href, data=file, timeout=5)
-        # response.raise_for_status()
-        # if response.status_code == 201:
-        #     print("Success")
+        response.raise_for_status()
+        if response.status_code != 201:
+            print("Ошибка загрузки!")
 
     def _get_headers(self):
         return {
@@ -27,7 +29,12 @@ class YaUploader:
         headers = self._get_headers()
         params = {"path": disk_file_path, "overwrite": "true"}
         response = requests.get(upload_url, headers=headers, params=params, timeout=5)
-        return response.json()
+        response.raise_for_status()
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print('Не удалось получить ссылку для загрузки фото!')
+            return {}
 
 
 class VkPhotoGetter:
@@ -48,35 +55,45 @@ class VkPhotoGetter:
             'v': self.version
         }
         response = requests.get(url, params=params, timeout=5)
+        response.raise_for_status()
+        if response.status_code != 200:
+            print('Не удалось получить список фотографий из Vk')
+            return []
         for photo in response.json()['response']['items']:
-            int_max_size = 0
-            target_size = photo['sizes'][0]
-            # ищем для каждой фотографии вариант максимального размера и оставляем только его
-            for size in photo['sizes']:
-                int_current_size = size['height'] * size['width']
-                if int_current_size > int_max_size:
-                    int_max_size = int_current_size
-                    target_size = size
-            photo.pop('sizes', '')
-            photo['size'] = target_size['height'] * target_size['width']
-            photo['url'] = target_size['url']
-            photo['date'] = datetime.fromtimestamp(photo['date']).strftime('%d-%m-%Y')
-
-            # присваиваем фотографии в качестве имени количество лайков. Если значения повторяются,
-            # добавляем к имени дату
-            likes_repetition = 0
-            for item in response.json()['response']['items']:
-                if photo['likes']['count'] == item['likes']['count']:
-                    likes_repetition += 1
-            if likes_repetition > 1:
-                photo['name'] = str(photo['likes']['count']) + '_' + photo['date']
-            else:
-                photo['name'] = str(photo['likes']['count'])
-
-            photo_list.append(photo)
-        # сортируем список фотографий по размеру в пикселях, чтобы скачивать фотографии по порядку согласно ФТ
-        sorted_photo_list = sorted(photo_list, key=lambda item: item['size'], reverse=True)
+            processed_photo = self._find_max_photo(photo)
+            processed_photo = self._to_name_photo(response, processed_photo)
+            photo_list.append(processed_photo)
+            # сортируем список фотографий по размеру в пикселях, чтобы скачивать фотографии по порядку согласно ФТ
+            sorted_photo_list = sorted(photo_list, key=lambda item: item['size'], reverse=True)
         return sorted_photo_list
+
+    def _find_max_photo(self, photo):
+        # ищем для каждой фотографии вариант максимального размера и оставляем только его
+        int_max_size = 0
+        target_size = photo['sizes'][0]
+        for size in photo['sizes']:
+            int_current_size = size['height'] * size['width']
+            if int_current_size > int_max_size:
+                int_max_size = int_current_size
+                target_size = size
+        photo.pop('sizes', '')
+        photo['size'] = target_size['height'] * target_size['width']
+        photo['url'] = target_size['url']
+        photo['date'] = datetime.fromtimestamp(photo['date']).strftime('%d-%m-%Y')
+        return photo
+
+    def _to_name_photo(self, response, photo):
+        # присваиваем фотографии в качестве имени количество лайков. Если значения повторяются,
+        # добавляем к имени дату
+        likes_repetition = 0
+        for item in response.json()['response']['items']:
+            if photo['likes']['count'] == item['likes']['count']:
+                likes_repetition += 1
+        if likes_repetition > 1:
+            photo['name'] = str(photo['likes']['count']) + '_' + photo['date']
+        else:
+            photo['name'] = str(photo['likes']['count'])
+        return photo
 
 
 def download_photo_from_vk(photo_list):
@@ -103,7 +120,7 @@ def download_photo_from_vk(photo_list):
             f.write(str(info_list))
 
 
-def put_photo_on_ya_disk(uploader, path='/api_vk/'):
+def put_photo_on_ya_disk_from_pc(uploader, path='/api_vk/'):
     files = os.listdir('pictures/')
     bar = IncrementalBar('Осталось ', max=len(files))
     for file in files:
@@ -111,6 +128,30 @@ def put_photo_on_ya_disk(uploader, path='/api_vk/'):
             uploader.upload(path + file, f)
         bar.next()
     bar.finish()
+
+
+def put_photo_on_ya_disk_from_vk(photo_list, uploader, path='/api_vk/'):
+    try:
+        quantity = int(input('Введите количество фото для загрузки на Яндекс Диск: '))
+    except TypeError:
+        print('Нужно ввести целое число!')
+    if quantity < 1:
+        pass
+    else:
+        bar = IncrementalBar('Осталось ', max=quantity)
+        info_list = []
+        for photo in photo_list[0:quantity]:
+            info_dict = {}
+            # with open('pictures/' + photo['name'] + '.jpg', 'wb') as f:
+            picture = requests.get(photo['url'])
+            uploader.upload(path + photo['name'] + '.jpg', picture.content)
+            info_dict['file_name'] = photo['name'] + '.jpg'
+            info_dict['size'] = photo['size']
+            info_list.append(info_dict)
+            bar.next()
+        bar.finish()
+        with open('info.json', 'w') as f:
+            f.write(str(info_list))
 
 
 def clear_directory(directory):
@@ -135,7 +176,8 @@ if __name__ == '__main__':
         1 - скачать фото из ВК в папку \'pictures\'
         2 - загрузить фото из папки \'pictures\' на Яндекс Диск
         3 - очистить папку \'pictures\'
-        4 - выйти из программы'''
+        4 - загрузить фото из ВК напрямую на Яндекс Диск
+        5 - выйти из программы'''
         print(text)
         command = input('Ввод: ')
 
@@ -143,10 +185,12 @@ if __name__ == '__main__':
             clear_directory('pictures/')
             download_photo_from_vk(photo_list)
         elif command == '2':
-            put_photo_on_ya_disk(uploader, path='/api_vk/')
+            put_photo_on_ya_disk_from_pc(uploader, path='/api_vk/')
         elif command == '3':
             clear_directory('pictures/')
         elif command == '4':
+            put_photo_on_ya_disk_from_vk(photo_list, uploader, path='/api_vk/')
+        elif command == '5':
             break
         else:
             print('Введена некорректная команда!')
